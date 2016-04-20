12a47f4b5db990adadf76d254494806d476d89ef
ckelner <chris.kelner@gmail.com>
2016-04-20 17:29:01 -0400

    email and error handling updates/fixes + debug

--- github_monitor.py
+++ github_monitor.py
@@ -23,66 +23,104 @@ from github_monitor.custom_exceptions import GitHubHTTPException
 REPO_WHITELIST_FILENAME = 'public_repos_whitelist.json'
 OUTSIDE_COLLABORATORS_WHITELIST_FILENAME = 'outside_collaborators_whitelist.json'
 ERRORS = []
+EMAIL_BODY = 'The following issues were found in the TWC GitHub Org:\n\n'
 
-def reconcileGitHubOutsideCollaborators(gh_org, skip):
+def reconcileGitHubOutsideCollaborators(gh_org, skip, debug):
+  global ERRORS
+  global EMAIL_BODY
   if skip is False:
     print 'Reconciling outside collaborators...'
+    outside_collabs = set()
     # @ckelner: more DRY? better way?
-    gh_errs = False
     try:
       members = gh_org.getAllOrgMembers() #set
     except GitHubHTTPException as e:
-      print e
-      ERRORS = ERRORS + e
-      gh_errs = True
+      ERRORS.append(str(e) + " -- during execution of reconcileGitHubOutsideCollaborators while calling github.getAllOrgMembers")
     try:
       repos = gh_org.getAllOrgRepos() #dict
     except GitHubHTTPException as e:
-      print e
-      ERRORS = ERRORS + e
-      gh_errs = True
-    if gh_errs is False:
-      print 'Processing GitHub Repos for outside collaborators...'
+      ERRORS.append(str(e) + " -- during execution of reconcileGitHubOutsideCollaborators while calling github.getAllOrgRepos")
+    try:
+      whitelist = parseWhitelist(OUTSIDE_COLLABORATORS_WHITELIST_FILENAME)
+    # @ckelner: not try... but need to capture to error to send via email
+    except IOError as e:
+      # @ckelner: a bit fugly... but considering we send it via email should be slightly helpful?
+      # TODO: Improve?
+      ERRORS.append(str(e) + " -- during execution of reconcileGitHubOutsideCollaborators while calling parseWhitelist")
+      return
+    except ValueError as e:
+      ERRORS.append(str(e) + " -- during execution of reconcileGitHubOutsideCollaborators while calling parseWhitelist")
+      return
+    print 'Processing GitHub Repos for outside collaborators...'
+    first_find = True
+    if debug:
+      print 'In debug mode, setting false data, skipping calling all repos...'
+      outside_collabs.add('debug-test-user')
+    else:
       for repo in tqdm(repos):
         repo_collaborators = gh_org.getCollaboratorsForRepo(repo)
         if repo_collaborators is not None and len(repo_collaborators) != 0:
           for collaborator in repo_collaborators:
             if collaborator['login'] not in members:
-              print 'Found outside collaborator %s' % collaborator['login']
-      # TODO: parse in whitelist
-      # TODO: add to email body
+              if collaborator['login'] not in outside_collabs:
+                outside_collabs.add(collaborator['login'])
+    for collab in outside_collabs:
+      if collab not in whitelist:
+        if first_find:
+          first_find = False
+          EMAIL_BODY += 'Outside Collaborators not in whitelist:\n'
+        EMAIL_BODY += '- %s\n' % collab
+    if first_find is False:
+      EMAIL_BODY += '\nPlease remove the collaborators for the TWC Org.\n\n'
   else:
     print 'Skipping reconciling outside collaborators'
 
 def checkPublicSourceReposAgainstWhitelist(gh_org, skip):
+  global ERRORS
+  global EMAIL_BODY
   if skip is False:
     print 'Checking for public repositories...'
-    gh_errs = False
     try:
       publicSourceRepos = gh_org.getPublicSourceRepos()
     except GitHubHTTPException as e:
-      print e
-      ERRORS = ERRORS + e
-      gh_errs = True
-    if gh_errs is False:
+      ERRORS.append(str(e) + " -- during execution of reconcileGitHubOutsideCollaborators while calling github.getAllOrgRepos")
+      return
+    try:
       whitelist = parseWhitelist(REPO_WHITELIST_FILENAME)
-      for repo in publicSourceRepos:
-        if repo not in whitelist:
-          # TODO: add to email body
-          print repo
+    except IOError as e:
+      ERRORS.append(str(e) + " -- during execution of checkPublicSourceReposAgainstWhitelist while calling parseWhitelist")
+      return
+    except ValueError as e:
+      ERRORS.append(str(e) + " -- during execution of checkPublicSourceReposAgainstWhitelist while calling parseWhitelist")
+      return
+    for repo in publicSourceRepos:
+      if repo not in whitelist:
+        # TODO: add to email body
+        print repo
   else:
     print 'Skipping check for publics repositories'
 
+# can throw: ValueError
+# can throw: IOError
 def parseWhitelist(which_list):
   whitelist_set = set()
-  try:
-    with open(which_list) as json_file:
-      json_data = json.load(json_file)
-      for data in json_data.get('data'):
-        whitelist_set.add(data)
-      return whitelist_set
-  except IOError:
-    sys.exit(formatIOError())
+  # @ckelner 04/20: Handle file exceptions in calling method, pass error back via email
+  with open(which_list) as json_file:
+    json_data = json.load(json_file)
+    for data in json_data.get('data'):
+      whitelist_set.add(data)
+  return whitelist_set
+
+def handleErrors(debug):
+  global EMAIL_BODY
+  if debug:
+    print '# of errors while running script: %s' % str(len(ERRORS))
+  if len(ERRORS) is not 0 and ERRORS is not None:
+    EMAIL_BODY = 'There were errors while reconciling GitHub.\n'
+    EMAIL_BODY += '\nThe errors are as follows: \n'
+    for err in ERRORS:
+      EMAIL_BODY += '- %s\n' % err
+    EMAIL_BODY += 'Please investigate.\n'
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
@@ -100,13 +138,22 @@ if __name__ == '__main__':
     action='store_true', default=False)
   parser.add_argument('-p', '-skip_public_repos', help='Skips checking publics repos',
     action='store_true', default=False)
+  parser.add_argument('-d', '-debug', help='Enables debug mode for development',
+    action='store_true', default=False)
   args = parser.parse_args()
   compileall.compile_dir('github_monitor/', force=True)
   aws_ses = amazon(args.aws_access_key_id, args.aws_secret_access_key, args.email_list)
   gh_org = github(args.k, args.o)
-  reconcileGitHubOutsideCollaborators(gh_org, args.s)
+  if args.d:
+    print '!' * 25
+    print '---- In debug mode ----'
+    print '!' * 25
+  reconcileGitHubOutsideCollaborators(gh_org, args.s, args.d)
   checkPublicSourceReposAgainstWhitelist(gh_org, args.p)
-  # TODO: handle ERRORS
+  handleErrors(args.d)
+  if args.d:
+    print 'DEBUG: printing email body:\n'
+    print EMAIL_BODY
   # TODO: add email call
   # TODO: GitHub Billing
   print 'Reconciliation complete.'
