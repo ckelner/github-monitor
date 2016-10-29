949ba3b1064c5ba9bd7f54fbd66f22795b1ca3ff
Chris Kelner <ckelner@users.noreply.github.com>
2016-10-29 12:22:55 -0400

    More logging

--- github_monitor/github.py
+++ github_monitor/github.py
@@ -43,11 +43,12 @@ class github(object):
   def githubGet(self, url, data=[]):
     response = requests.get(url, headers=self.HEADERS)
     if response.status_code is not self.OK:
+      print 'Recieved non-200 response code: %s' % response.status_code
       #kelnerhax - deal with it
       if response.status_code is self.FORBIDDEN:
         print response.json()
-        #print '!INVESTIGATE! 403 while querying %s' % url
-        #print 'Data dump:'
+        print '!INVESTIGATE! 403 while querying %s' % url
+        print 'Data dump:'
         print json.dumps(json.loads(data), indent=4, sort_keys=True)
       else: 
         raise GitHubHTTPException(self.formatInvalidHttpStatusMessage(url, response.status_code))
